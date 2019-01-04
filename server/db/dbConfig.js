476faac8ab9c6ff3d7cdf1abcfcb4947ffb251d6
Jawahar <jawaharkannan19@gmail.com>
2019-01-05 04:41:43 +0530

    Removed private Details

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-    dbuser: "krdsUser",
-    dbpwd: "krdsUser@123",
-    dbServer: "ds015760.mlab.com",
-    dbport: 15760,
+    dbuser: "*",
+    dbpwd: "*",
+    dbServer: "*",
+    dbport: 0,
     dbname: "todolist",
     url() {
         return `mongodb://${this.dbServer}:${this.dbport}/${this.dbname}`
     }
-    
+
 }
